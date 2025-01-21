9b9864871f74e2df582313276ef32c68a6a934c7
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2025-01-21 13:06:03 +0100

    new  projects created, index + basic styles created, mobile-menu added

---
+++ js/scroll-box.js
@@ -0,0 +1,6 @@
+const projectsList = document.querySelector(".projects__list");
+
+function createList() {
+  const element = document.createElement("li");
+  element.classList.add("projects__list-element");
+}
